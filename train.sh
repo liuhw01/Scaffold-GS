c89bbd724b4879732a355e30592310a18959e1bd
Hanwei Liu <67702642+liuhw01@users.noreply.github.com>
2025-06-16 19:07:12 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,11 +5,38 @@ function rand(){
     echo $(($num%$max+$min))  
 }
 
+# 调用 rand 函数，在 10000~30000 之间生成一个端口号（避免端口冲突）。
 port=$(rand 10000 30000)
 
+
+# 默认的 level-of-detail 为 0；
+# 训练迭代次数为 30,000；
+# 默认不启用 warmup 预热。
 lod=0
 iterations=30_000
 warmup="False"
+
+# 支持传入的参数有：
+
+# --logdir: 输出目录名（日志等）
+
+# --data: 数据集名称（或路径下的子目录）
+
+# --lod: LOD 级别（默认 0）
+
+# --gpu: GPU ID
+
+# --warmup: 是否开启 warmup（True / False）
+
+# --voxel_size: 体素大小
+
+# --update_init_factor: 锚点更新因子
+
+# --appearance_dim: 外观特征维度
+
+# --ratio: 某个控制比例（比如 anchor 数量占比）
+
+# 若遇到未知参数，则退出。
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -l|--logdir) logdir="$2"; shift ;;
